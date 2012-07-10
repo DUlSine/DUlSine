58d08a023a7509ba460df90e9e626a455c3cfdca
Rémi Duraffort <ivoire@videolan.org>
2012-07-10 15:44:27 +0200

    Ajout de la délégation de ratachement d'un intervenant.

--- models/intervenant.py
+++ models/intervenant.py
@@ -1,5 +1,9 @@
 from django.db import models
 
+from delegation import Delegation
+
 class Intervenant(models.Model):
     class Meta:
         app_label = 'DUlSine'
+
+    delegation = models.ForeignKey(Delegation)
