0432a4dc991f97b2dd24e6a7b1502dcc78323258
Rémi Duraffort <ivoire@videolan.org>
2012-07-18 08:09:43 +0200

    Ajout de l'interface d'admin.

---
+++ admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from models import *
+
+admin.site.register(DPS)
+admin.site.register(Dimenssionnement)
+admin.site.register(PAPS)
+admin.site.register(Equipe)
+admin.site.register(Binome)
+admin.site.register(Delegation)
+admin.site.register(Organisateur)
+admin.site.register(Contact)
