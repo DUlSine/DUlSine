e72eee68d912ffd25988165ff04dca0c79eea908
Rémi Duraffort <ivoire@videolan.org>
2013-01-01 17:28:20 +0100

    Models: add three models for DPS subscriptions

--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,4 @@ from benevole import *
 from DPS import *
 from organisateur import *
 from structure import *
+from team import *
---
+++ models/team.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+# vim: set ts=4
+from django.db import models
+
+from benevole import Benevole
+from DPS import Dimensionnement
+
+
+TEAM_TYPES = (
+    (0, 'PAPS'),
+    (1, 'Equipe'),
+    (2, 'Binome'),
+    (3, 'Equipe d\'Evacuation')
+)
+
+
+FORMATIONS = (
+    ('CI',      'Chef d\'Intervention'),
+    ('PSE2',    'Equipier Secouriste'),
+    ('PSE1',    'Secouriste'),
+    ('PSC1',    'Sauveteur'),
+    ('IPS',     'IPS'),
+
+    ('CH',      'Chauffeur'),
+    ('CHA',     'Chauffeur Ambulance')
+)
+
+
+
+class Team(models.Model):
+    class Meta:
+        app_label = 'DUlSine'
+
+    dimensionnement = models.ForeignKey(Dimensionnement)
+    team_type = models.IntegerField(choices = TEAM_TYPES)
+    taille = models.IntegerField()
+
+    def __unicode__(self):
+        return str(self.dimensionnement) + ' (' + TEAM_TYPES[self.team_type][1] + ')'
+
+
+
+class Souhait(models.Model):
+    class Meta:
+        app_label = 'DUlSine'
+
+    benevole = models.ForeignKey(Benevole)
+    dimensionnement = models.ForeignKey(Dimensionnement)
+    fonction = models.CharField(max_length = 6, choices = FORMATIONS)
+    date = models.DateTimeField()
+
+    def __unicode__(self):
+        return str(self.dimensionnement) + ' => ' + self.fonction + ' : ' + str(self.benevole)
+
+
+
+class Inscription(models.Model):
+    class Meta:
+        app_label = 'DUlSine'
+
+    benevole = models.ForeignKey(Benevole)
+    team = models.ForeignKey(Team)
+    fonction = models.CharField(max_length = 6, choices = FORMATIONS)
+
+    def __unicode__(self):
+        return str(self.team) + ' => ' + self.fonction + ' : ' + str(self.benevole)
+
