9e5528e56cd7987eacba945f581859b06410aac8
Rémi Duraffort <ivoire@videolan.org>
2013-01-13 14:18:26 +0100

    Add a page to see all the benevole of a structure

--- views/benevole.py
+++ views/benevole.py
@@ -1,9 +1,18 @@
 # -*- coding: utf-8 -*-
 # vim: set ts=4
 from django.http import HttpResponse
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
+
+from DUlSine.models import Structure, Benevole
 
 def index(request, structure):
-    return HttpResponse(status = 200)
+    Struct = get_object_or_404(Structure, numero = structure)
+    benevoles = Benevole.objects.filter(structure = structure)
+
+    return render_to_response('benevole/index.html', {'structure': Struct, 'benevoles': benevoles}, context_instance = RequestContext(request))
+
+
 
 def details(request, benevole_id):
     return HttpResponse(status = 200)
