5b51968d0ec775a3b77e1c05cb12c35a08c968ea
Adrien Maglo <magsoft@videolan.org>
2013-01-20 22:09:49 +0100

    Add names to the benevole fields.

--- models/benevole.py
+++ models/benevole.py
@@ -14,10 +14,10 @@ class Benevole(models.Model):
         return u"%s (%s)" %(self.user.get_full_name(), self.structure.nom)
 
     user = models.OneToOneField(User)
-    structure = models.ForeignKey(Structure)
+    structure = models.ForeignKey(Structure, verbose_name='structure de ratachement')
 
     # TODO: use https://docs.djangoproject.com/en/dev/ref/contrib/localflavor/
-    telephone = models.IntegerField()
-    adresse = models.CharField(max_length = 200)
-    date_naissance = models.DateField()
-    diplomes = models.CommaSeparatedIntegerField(max_length = 200)
+    telephone = models.IntegerField('téléphone')
+    adresse = models.CharField('adresse', max_length = 200)
+    date_naissance = models.DateField('date de naissance')
+    diplomes = models.CommaSeparatedIntegerField('diplômes', max_length = 200)
