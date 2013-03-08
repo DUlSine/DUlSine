3a8aa3f471ade5a57c1afc1fbe12e449bf3e14a1
Rémi Duraffort <ivoire@videolan.org>
2013-03-08 17:54:19 +0100

    Test: remove outdated tests

--- tests.py
+++ tests.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 # vim: set ts=4
-from django.utils import unittest
-from django.test.client import Client
 
-from DUlSine.models.DPS import Dimensionnement
+
+from django.test import TestCase
+from django.test.client import Client
+from django.utils import unittest
+
+from DUlSine.models import *
 
 
 class URLTest(unittest.TestCase):
@@ -14,19 +17,14 @@ class URLTest(unittest.TestCase):
         self.assertEqual(self.client.get('/DUlSine/DPS/').status_code, 404)
 
         self.assertEqual(self.client.get('/DUlSine/38/DPS').status_code, 301)
-        self.assertEqual(self.client.get('/DUlSine/38/DPS').status_code, 301)
+        self.assertEqual(self.client.get('/DUlSine/38/DPS/').status_code, 200)
         self.assertEqual(self.client.get('/DUlSine/arrt/DPS/').status_code, 404)
 
         self.assertEqual(self.client.get('/DUlSine/3802/DPS/123456').status_code, 301)
-        self.assertEqual(self.client.get('/DUlSine/3802/DPS/123456/').status_code, 200)
-        self.assertEqual(self.client.get('/DUlSine/3802/DPS/12razt/').status_code, 404)
+        self.assertEqual(self.client.get('/DUlSine/3802/DPS/123456/').status_code, 404)
 
-        self.assertEqual(self.client.get('/DUlSine/3802/DPS/nouveau').status_code, 301)
-        self.assertEqual(self.client.get('/DUlSine/3802/DPS/nouveau/').status_code, 200)
-        self.assertEqual(self.client.get('/DUlSine/3802/DPS/nouveau/123456ref1231').status_code, 301)
-        self.assertEqual(self.client.get('/DUlSine/3802/DPS/nouveau/123456ref1231/').status_code, 200)
-        self.assertEqual(self.client.get('/DUlSine/3802/DPS/nouveau/123456ref1231/12345').status_code, 301)
-        self.assertEqual(self.client.get('/DUlSine/3802/DPS/nouveau/123456ref1231/12345/').status_code, 200)
+        self.assertEqual(self.client.get('/DUlSine/3802/DPS/demande').status_code, 301)
+        self.assertEqual(self.client.get('/DUlSine/3802/DPS/demande/').status_code, 200)
 
         self.assertEqual(self.client.get('/DUlSine/3802/DPS/calendrier').status_code, 301)
         self.assertEqual(self.client.get('/DUlSine/3802/DPS/calendrier/').status_code, 200)
@@ -39,19 +37,9 @@ class URLTest(unittest.TestCase):
         self.assertEqual(self.client.get('/DUlSine/3802/DPS/calendrier/562/981/123').status_code, 404)
 
     def test_benevole(self):
-        self.assertEqual(self.client.get('/DUlSine/1230/benevole').status_code, 301)
-        self.assertEqual(self.client.get('/DUlSine/1230/benevole/').status_code, 200)
-
-        self.assertEqual(self.client.get('/DUlSine/benevole/calendrier').status_code, 404)
-        self.assertEqual(self.client.get('/DUlSine/benevole/4212/calendrier').status_code, 301)
-        self.assertEqual(self.client.get('/DUlSine/benevole/4212/calendrier/').status_code, 200)
-        self.assertEqual(self.client.get('/DUlSine/benevole/4212/calendrier/format').status_code, 301)
-        self.assertEqual(self.client.get('/DUlSine/benevole/4212/calendrier/format/').status_code, 200)
-        self.assertEqual(self.client.get('/DUlSine/benevole/4212/calendrier/format/4567').status_code, 301)
-        self.assertEqual(self.client.get('/DUlSine/benevole/4212/calendrier/format/4567/12').status_code, 301)
-        self.assertEqual(self.client.get('/DUlSine/benevole/4212/calendrier/format/4567/12/').status_code, 200)
-        self.assertEqual(self.client.get('/DUlSine/benevole/4212/calendrier/format/4567/12/12').status_code, 404)
-        self.assertEqual(self.client.get('/DUlSine/benevole/4212/calendrier/format/4567/12/erat').status_code, 404)
+        self.assertEqual(self.client.get('/DUlSine/1230/benevoles').status_code, 301)
+        self.assertEqual(self.client.get('/DUlSine/1230/benevoles/').status_code, 404)
+        self.assertEqual(self.client.get('/DUlSine/3810/benevoles/').status_code, 200)
 
 
 class DimensionnementTest(unittest.TestCase):
