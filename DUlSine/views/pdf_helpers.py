154f1d474148b2f12e5fae9056b8317b633c1843
Rémi Duraffort <ivoire@videolan.org>
2013-03-16 11:44:47 +0100

    Devis: fill some informations

--- DUlSine/views/pdf_helpers.py
+++ DUlSine/views/pdf_helpers.py
@@ -19,7 +19,9 @@ def generate_devis(buff, dps):
   # Create the document
   doc = SimpleDocTemplate(buff, pagesize = A4,
                           rightMargin = 72, leftMargin = 72, topMargin = 72, bottomMargin = 18,
-                          title = "Devis - DPS du XX/XX/XXXX", author = "Croix-Rouge Francaise - UL de XXX", subject = "Devis")
+                          title = "Devis - DPS du XX/XX/XXXX",
+                          author = "Croix-Rouge Francaise - UL de %s" % (dps.structure.nom),
+                          subject = "Devis")
 
   # Add elements to print in one raw at the end
   elements.append(Paragraph("XXX, le XXX XXX XXX", ParagraphStyle(name = 'Date', alignment = TA_RIGHT)))
@@ -32,26 +34,26 @@ def generate_devis(buff, dps):
   #elements.append(I)
 
   elements.append(Spacer(1, 20))
-  elements.append(Paragraph("Monsieur XXX XXX", ParagraphStyle(name = 'destinataire', alignment = TA_RIGHT)))
-  elements.append(Paragraph("XXX organisateur XXX", ParagraphStyle(name = 'destinataire', alignment = TA_RIGHT)))
+  elements.append(Paragraph(dps.organisateur.contact(), ParagraphStyle(name = 'destinataire', alignment = TA_RIGHT)))
+  elements.append(Paragraph(dps.organisateur.nom, ParagraphStyle(name = 'destinataire', alignment = TA_RIGHT)))
   elements.append(Spacer(1, 5))
-  elements.append(Paragraph("XXX adresse organisateur XXX", ParagraphStyle(name = 'destinataire', alignment = TA_RIGHT)))
+  elements.append(Paragraph(dps.organisateur.adresse, ParagraphStyle(name = 'destinataire', alignment = TA_RIGHT)))
 
   # Object
   identFirstLine = ParagraphStyle(name ='paragraph', firstLineIndent = 12)
   elements.append(Spacer(1, 20))
   elements.append(Paragraph("<u>Objet</u> : Devis suite à une demande de poste de secours", styles['Normal']))
   elements.append(Spacer(1, 20))
-  elements.append(Paragraph("XXX Monsieur, Madame, XXX", identFirstLine))
+  elements.append(Paragraph("%s," % (dps.organisateur.contact_civilite), identFirstLine))
   elements.append(Paragraph("Suite à votre demande à l'occasion de la manifestation :", styles['Normal']))
 
   # The title
   centered = ParagraphStyle(name = 'centered', alignment = TA_CENTER)
   elements.append(Spacer(1, 5))
-  elements.append(Paragraph("<b><u>XXX nom DPS XXX</u></b>", centered))
+  elements.append(Paragraph("<b><u>%s</u></b>" % (dps.intitule), centered))
   elements.append(Paragraph("Du XXX au XXX, de XXX à XXX", centered))
   elements.append(Paragraph("Qui se déroule à l'endroit suivant :", centered))
-  elements.append(Paragraph("XXX", centered))
+  elements.append(Paragraph(dps.lieu, centered))
 
   # List the persons
   ident = ParagraphStyle(name = 'listing', leftIndent = 30)
@@ -82,15 +84,15 @@ def generate_devis(buff, dps):
   elements.append(Paragraph("Nous rappellons que les secouristes sont bénévoles et ne touchent aucune indemnités. Nous \
   espèrons que vous ferez appel à nos services et à sa qualité.", identFirstLine))
   elements.append(Spacer(1, 20))
-  elements.append(Paragraph("Dans l’attente d’une confirmation écrite de votre part, veuillez agréer, Monsieur, mes salutations distinguées.", identFirstLine))
+  elements.append(Paragraph(u'Dans l’attente d’une confirmation écrite de votre part, veuillez agréer, Monsieur, mes salutations distinguées.', identFirstLine))
 
 
   # Sign it
   elements.append(Spacer(1, 30))
   elements.append(Paragraph("XXX Auteur XXX", centered))
   elements.append(Paragraph("XXX Titre XXX", centered))
-  elements.append(Paragraph("Croix-Rouge Française", centered))
-  elements.append(Paragraph("UL de XXX", centered))
+  elements.append(Paragraph(u'Croix-Rouge Française', centered))
+  elements.append(Paragraph(u"Unitée Locale de %s" % (dps.structure.nom), centered))
 
   # Write the document to the buffer
   doc.build(elements)
